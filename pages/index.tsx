50a1297b148daf534514a37eb473cfbb595ce886
Dan Tavelli <daniel.tavelli@gmail.com>
2023-05-03 14:30:39 -0400

    chore: text updates and nav added

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import { Righteous } from "next/font/google";
 import { format, parse, parseISO } from "date-fns";
 
 import { request } from "../lib/datocms";
-import bumpsLogo from "../public/bumpslogo_transparent.png";
+import bumpsLogo from "../public/bumps-logo.png";
 
 interface HomepageQuery {
   allEvents: Event[];
@@ -50,13 +50,29 @@ export default function Home({
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <main
-      className={`justify-between md:p-8 lg:p-16 min-h-screen ${righteous.className}`}
+      className={`justify-between md:p-8 lg:p-10 min-h-screen ${righteous.className}`}
     >
       <header className="flex flex-col items-center pb-8 md:pb-16 pt-8 md:pt-0">
-        <Image src={bumpsLogo} alt="BUMPS Logo" width={200} />
+        <Image
+          src={bumpsLogo}
+          alt="Bike up Mountain Point Series Logo"
+          width={250}
+          className="pl-12 pr-12 md:pl-0 md:pr-0"
+        />
         <h1 className="sr-only">BUMPS</h1>
         <p className="text-xl sr-only">Bike Up the Mountain Points Series</p>
       </header>
+      <nav className="nav grid gap-4 md:grid-cols-3 pb-8 pt-8 justify-items-center">
+        <a href="#overview">Overview</a>
+        <a href="#points">Points System</a>
+        <a
+          target="_blank"
+          rel="noopener"
+          href="https://www.road-results.com/BUMPS"
+        >
+          Series Results
+        </a>
+      </nav>
       <section className="hill-listing">
         {data.allEvents.reverse().map((event) => (
           <div key={event.title} className="relative hill-wrapper">
@@ -87,27 +103,16 @@ export default function Home({
       >
         <h2>Overview</h2>
         <p>
-          The BUMPS (Bike Up the Mountain Points Series) is a series that
-          recognizes the talents of riders who do the hardest thing in cycling,
-          climb steep hills.
+          BUMPS, short for Bike Up the Mountain Points Series, is a yearlong
+          competition featuring some of the most challenging and
+          well-established hill climb events in the Northeast United States.
         </p>
-
         <p>
-          Any rider who enters a BUMPS event will automatically be entered into
-          the overall series points standings. There is no need to enter a
-          separate BUMPS category. Riders will be scored according to their
-          accumulated points in up to five races, and the totals of riders who
-          enter more than five races will be determined by their best five
-          scores. The official standings are on RoadResults:
-          <a
-            target="_blank"
-            rel="noopener"
-            href="https://www.road-results.com/BUMPS"
-          >
-            https://www.road-results.com/BUMPS
-          </a>
+          Entering a BUMPS event includes automatic entry into the overall
+          series standings. Riders accumulate points in up to three races, and
+          those entering more than three races are scored based on their best
+          three results.
         </p>
-
         <p>
           Each event will continue to be run by an independent organizer, and
           event formats and prizes will continue to be determined independently.
@@ -130,34 +135,28 @@ export default function Home({
           Example: In a race where the fastest time is 1:00 and the average
           finish time is 1:30: Finishing in 1:15 earns 75 points, 1:30 earns 50
           points, 1:45 earns 25 points, and finish times of 2:00 or slower earn
-          one point). This scoring system values fast times, like a time trial,
-          not your place.
+          one point.
+        </p>
+        <p>
+          The scoring system prioritizes fast times, similar to a time trial,
+          over your finishing position.
         </p>
 
         <p>
-          (In past years there was a 1.2 multiplier for the Mt. Washington Auto
-          Road Bicycle Hillclimb. In 2021 we have decided to eliminate that
-          bonus. Ultimately, the goal of BUMPS is to encourage participation is
-          an many events as possible. By removing the multiplier, participants
-          who were unable to enter Mt. Washington can make up for it more easily
-          by joining other events.){" "}
+          Each event will utilize its own timing system to determine category
+          winners and distribute event-specific awards. The results of each race
+          will subsequently be incorporated into the BUMPS scoring system, with
+          the best three race scores counting towards each rider&apos;s total.
+          Participation in any of the series events automatically qualifies
+          racers for the BUMPS series.
         </p>
-      </section>
-      <section id="points" className="prose body-text mt-0 p-8 md:p-0 md:mt-12">
-        <h2>Series Scoring & Results</h2>
+        <h3>Categories</h3>
         <p>
-          As noted each event will use their own timing system to establish
-          winners in each category and provide their individual event awards.
-          The race results will then be applied to the BUMPS scoring system
-          using the 5 highest race scores for each rider. Every racer competing
-          in any of the series races is automatically entered into the BUMPS
-          series and will be scored. In addition to awarding overall male and
-          female series leaders the following age categories will be scored:
-        </p>
-        <p>
-          Male: Under 20, 20-29, 30-39, 40-49, 50-59, 60-69, 70-74, 75-79, 80+
+          Male: Overal, Under 20, 20-29, 30-39, 40-49, 50-59, 60-69, 70-74,
+          75-79, 80+
           <br />
-          Female: Under 20, 20-29, 30-39, 40-49, 50-59, 60-69, 70-74, 75-79, 80+
+          Female: Overall, Under 20, 20-29, 30-39, 40-49, 50-59, 60-69, 70-74,
+          75-79, 80+
           <br />
           Overall unicycle
           <br />
@@ -165,22 +164,18 @@ export default function Home({
           <br />
         </p>
         <p>
-          * Please note age categories are assigned based on a riders age at the
-          end of the year. It is possible that your age category in individual
-          races is different than for the BUMPS series. * Competitors in
-          Clydesdale and Filly categories in individual races will be included
-          in the BUMPS results but they will be assigned to their appropriate
-          age category in the BUMPS scoring. BUMPS does not have a Clydesdale or
-          Filly category.
+          * Age categories are determined by a rider&apos;s age at the end of
+          the year. Please note that your age category for individual races may
+          differ from that of the BUMPS series.
         </p>
         <p>
-          Results will be listed on-line after each event at RoadResults:{" "}
+          Series Results:{" "}
           <a
             target="_blank"
             rel="noopener"
             href="https://www.road-results.com/BUMPS"
           >
-            https://www.road-results.com/BUMPS
+            road-results.com/BUMPS
           </a>
           .
         </p>
